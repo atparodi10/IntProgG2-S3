cdb68b08387ffd434b969cf013ecb1236a57200f
atparodi10 <joatorres@uamv.edu.ni>
2025-04-01 11:22:47 -0600

    Ejercicios de práctica Python

---
+++ Python-S2/temp.py
@@ -0,0 +1,8 @@
+tem_fah = float(input("Ingrese la temperaura en grados Fahrenheit: "))
+temp_cel = tem_fah - 32
+temp_cel = temp_cel * 5
+temp_cel = temp_cel / 9
+temp_kel = temp_cel + 273.15
+
+print(f"{temp_cel: .2f}")
+print(f"{temp_kel: .2f}")
